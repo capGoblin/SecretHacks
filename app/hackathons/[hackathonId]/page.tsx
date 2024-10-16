abb045afa664addd100bb2d6899e5991c55ca07d
capGoblin <dharshan2457@gmail.com>
2024-10-16 16:44:42 +0530

    init

--- app/hackathons/[hackathonId]/page.tsx
+++ app/hackathons/[hackathonId]/page.tsx
@@ -138,10 +138,8 @@ export default function HackathonDetailPage({
     const currentDate = new Date();
     const targetDate = new Date(futureDate);
 
-    // Calculate the difference in milliseconds
     const diffTime = targetDate.getTime() - currentDate.getTime();
 
-    // Convert milliseconds to days (1000 ms/s * 60 s/min * 60 min/hr * 24 hr/day)
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
     return diffDays;
